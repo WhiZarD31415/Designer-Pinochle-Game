7e5cbbd89cf468c792ba38168c5716d7d42cf7d5
Connor Vitz <connorvitz@gmail.com>
2024-03-28 00:20:38 -0400

    Updating tests

--- TEST_CaDD.py
+++ TEST_CaDD.py
@@ -9,11 +9,13 @@ def test_decklist() -> bool:
     '''
     Testing for "create_deck_list_funct"
 
-    Checks if there are 96 cards in the appropriate global "list_of_cards". If there is not, it prints an
-    error with a description. It also checks if there is four of every card formatted correctly (all at one
-    time for simplicity). Checks this by counting every card into a 2D list with elements for every
-    corresponding card. If there is not, it prints an error with the 2D list for reference. At the end,
-    prints the eventual return boolean.
+    Checks:
+        - Total Cards: Pass if there are 96 cards in the appropriate global "list_of_cards".
+        - Validate Cards: Pass if every card is formatted correctly.
+        - Accounting: Pass if there is four of every card formatted correctly (all at one time
+        for simplicity). Checks this by counting every card into a 2D list with elements for every
+        corresponding card. If there is not, it prints an error with the 2D list for reference.
+    Then, prints the eventual return boolean.
 
     Return:
         bool: represents if the test passed all checks
@@ -44,7 +46,7 @@ def test_decklist() -> bool:
                   + "' was not in correct path string format")
             valid_card_bool = valid_card_bool and False
 
-    #Accounting for every card in correct format
+    #Accounting for every card
     values = ["9", "jack", "queen", "king", "10", "ace"]
     suits = ["clubs", "diamonds", "spades", "hearts"]
     twoD_count_list = [[0,0,0,0,0,0],
@@ -95,7 +97,18 @@ def test_deal_helper() -> bool:
     '''
     Testing for "deal_cards_helper"
 
-    Checks (Unfinished docstring)
+    Checks:
+        - Remaining Card Count: Pass if there the global 'remaining_card_count' is reduced correctly
+        each time a card is dealt.
+        - Player Card Count: Pass if the length of global 'player_cards' is increased correctly
+        each time a card is dealt.
+        -'list_of_cards' Card Count: Pass if there the global 'list_of_cards' is reduced correctly
+        each time a card is dealt.
+        - Validate Cards: Pass if every card is formatted correctly.
+    Then, prints the eventual return boolean.
+
+    Blindspots:
+        - Does not check to make sure the card removed from 'list_of_cards' == card added to 'player_hand'
 
     Return:
         bool: represents if the test passed all checks
@@ -105,10 +118,11 @@ def test_deal_helper() -> bool:
     passing_bool = True
     remaining_count_bool = True
     player_count_bool = True
+    list_of_card_count_bool = True
     valid_card_bool = True
     loop_number = 1
 
-    #Repeatability
+    #For repeatability
     while loop_number <= 16:
         deal_cards_helper(Global_Variables.player_cards)
 
@@ -127,6 +141,14 @@ def test_deal_helper() -> bool:
             print("    Error: Number of player cards after " + str(loop_number) + " deal(s) = "
                 + str(len(Global_Variables.player_cards)) + ". Not " + str(loop_number))
             player_count_bool = False
+
+        #"list_of_cards" Card Count
+        if len(Global_Variables.list_of_cards) == (96 - loop_number):
+            list_of_card_count_bool = list_of_card_count_bool and True
+        else:
+            print("    Error: 'list_of_cards' card count was " + str(len(Global_Variables.list_of_cards))
+                  + ". Not " + str(96 - loop_number))
+            list_of_card_count_bool = False
         loop_number += 1
 
     #Validate Cards
@@ -138,15 +160,27 @@ def test_deal_helper() -> bool:
                     + "' was not in correct path string format")
             valid_card_bool = False
 
-    #Conclusion
-    passing_bool = passing_bool and remaining_count_bool and player_count_bool and valid_card_bool
+    passing_bool = passing_bool and remaining_count_bool and player_count_bool and valid_card_bool and list_of_card_count_bool
     print("  Result = " + str(passing_bool))
     return passing_bool
 
+def test_deal_funct() -> bool:
+    #Hands Card Count
+    #Accounting for every card
+    #Test sorting elsewhere
+    return
 
-#Function calls
+#Tests 1 and 2
 test_decklist()
 test_deal_helper()
+
+#Reset Vars and Deal Again
+Global_Variables.remaining_card_count = 95
+Global_Variables.list_of_cards = []
+Global_Variables.player_cards = []
+create_deck_list_funct()
+test_deal_funct()
+
 print("")
 
 valid_cards.close()
